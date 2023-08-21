ba5fd737d4fbbdf02b14abe6697c9be048d9026f
Collins Chirchir <collinskchirchir@gmail.com>
2023-08-21 18:56:06 +0300

    basic pokemon card

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,24 @@ export default function Home() {
   return (
     <>      
       <main>
-        
+        <div className="all-pokemon">
+          {allPokemonData ? allPokemonData.map((pokemon) => {
+            return (
+              <div key={pokemon.id} className="pokemon">
+                <div className="card-image">
+                  <img 
+                    src={pokemon.sprites.other.home.front_shiny} 
+                    alt={pokemon.name}
+                  />
+                </div>
+                <div className="card-body">
+                  <h3>{pokemon.name}</h3>
+                  <p>More Details &nbsp; &rarr;</p>
+                </div>
+              </div>
+            )
+          }) : <h1>Loading...</h1>}
+        </div>
       </main>
     </>
   )
