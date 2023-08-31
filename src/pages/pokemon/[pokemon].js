e4e8c4c621b24b39b0681adb36aa1257289ca4a8
Collins Chirchir <collinskchirchir@gmail.com>
2023-08-31 20:52:51 +0300

    pokemon styling complete

--- src/pages/pokemon/[pokemon].js
+++ src/pages/pokemon/[pokemon].js
@@ -1,7 +1,7 @@
 import { useGlobalContext } from '@/context/global';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
-import styles from "@styles/Pokemon.module.css"
+import styles from "@/styles/Pokemon.module.css";
 
 function Pokemon() {
    const router = useRouter();
@@ -18,37 +18,54 @@ function Pokemon() {
       const {"official-artwork": link} = pokemonItem?.sprites?.other
       myLink = link.front_default
    }
+
+   // pokemon bg colors
+   const pkColors = [
+      "#a2d9ce",
+      "#f5b7b1",
+      "#c39bd3",
+      "#f5cba7",
+      "#85c1e9",
+      "#76d7c4"
+   ];
+
+   const randomColor = pkColors[Math.floor(Math.random() * pkColors.length)];
+
    console.log(myLink);      
    
    console.log(pokemonItem)
 
    return (
-      <>
+      <div className={styles.PokemonBg} style={{
+         background: !loading && randomColor
+      }}>
       {pokemonItem && (
          <>
-         <div>
+         <div className={styles.PokemonImage}>
             <img src={
                pokemonItem?.sprites?.other?.home.front_default
                ? pokemonItem?.sprites?.other?.home.front_default
                : myLink} alt='' />
          </div>
-         <div>
+         <div className={styles.PokemonBody}>
             <h2>{pokemonItem?.name}</h2>
-            <div>
-               <h5>Name: </h5>
-               <p>{pokemonItem?.name}</p>
-            </div>
-            <div>
-               <h5>Type: </h5>
-               <p>{pokemonItem?.types?.map((type) => {
-                  return type.type.name
-               })}</p>
+            <div className={styles.PokemonInfo}>
+               <div className={styles.PokemonInfoItem}>
+                  <h5>Name: </h5>
+                  <p>{pokemonItem?.name}</p>
+               </div>
+               <div className={styles.PokemonInfoItem}>
+                  <h5>Type: </h5>
+                  <p>{pokemonItem?.types?.map((type) => {
+                     return  <p key={type.type.name}>{type.type.name},</p>
+                  })}</p>
+               </div>
             </div>
          </div>
          
          </>
          )}
-      </>
+      </div>
    );
 }
 
